88f0c73cef1bead94f0c627a6ce11af0698d9709
CornWorld <cw@had.ink>
2025-06-28 21:50:46 +0800

    lab09: finish

--- Lab/lab09/lab09.scm
+++ Lab/lab09/lab09.scm
@@ -1,10 +1,17 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) (if (< num1 num2)
+                                    -1
+                                    (if (= num1 num2)
+                                      0
+                                      1)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) (lambda inc: (+ num inc)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) (lambda x:(f (g x))))
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n)
+  (if(> n 0)
+    (begin((f) (repeat f (- n 1))))
+    nil))
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +23,4 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) (if (!= b 0) (gcd b (% a b)) nil))
